3e290af64a9b9cf1512f57f62545135c7a27f54a
Templer <ericTempler@googlemail.com>
2020-02-01 21:23:31 +0100

    Updated upgrades.js

--- js/upgrades.js
+++ js/upgrades.js
@@ -1,28 +1,22 @@
 g.upgrades = g.u = {};
 g.u.owned = [];
 
-game.upgrades.create = function (name, desc, price, priceName, reqName, changeName, changeValue, type) {
+game.upgrades.create = function (name, desc, price, buyFunction) {
     this.name = name;
     this.desc = desc;
     this.price = price;
-    this.priceName = priceName;
-    this.reqName = reqName;
-    this.changeName = changeName;
-    this.changeValue = changeValue;
-    this.type = type;
+    this.func = buyFunction;
 };
+
 game.upgrades.buy = function (i) {
-    let a = eval("g.ressources." + g.u.list[i].reqName);
-    let index = g.ressources.list.indexOf(g.u.list[i].priceName);
-    
-    if (a >= g.u.list[i].price && g.u.owned[i] === false) {
-        let preStr = g.u.list[i].changeName.substring(0, g.u.list[i].changeName.indexOf('['));
-        let strIndex = g.u.list[i].changeName.substring(g.u.list[i].changeName.indexOf('[') + 1, g.u.list[i].changeName.length - 1);
-        let fullStr = "g.ressources." + preStr + '[' + strIndex + ']';
-        window["g"]["ressources"][preStr][strIndex] = eval(fullStr + g.u.list[i].changeValue);
-        g.ressources.owned[index] -= g.u.list[i].price;
+    let obj = g.u.list[i];
+
+    if (g.ressources.owned[obj.price.type] >= obj.price.amount && g.u.owned[i] === false) {
+        g.ressources.owned[obj.price.type] -= obj.price.amount;
+        obj.func();
         g.u.owned[i] = true;
         g.buttons();
+
         let upgradeBTN = $("#upgrades-btn-" + i);
         upgradeBTN.attr('onclick', '');
         upgradeBTN.removeClass('btn-primary');
@@ -31,13 +25,21 @@ game.upgrades.buy = function (i) {
         g.u.hide();
     }
 };
+
 game.upgrades.init = function () {
     for (let i = 0; i < g.u.list.length; i++) {
         g.u.owned.push(false);
-        $("#upgrades-panelbody").append('<div id="upgrades-row-' + i + '" class="row bottom-spacer">'
-            + '<div class="col-md-8"><p class="no-margin">' + g.u.list[i].name + " : " + g.u.list[i].desc + "<br>Cost " + fix(g.u.list[i].price, 0) + " " + g.u.list[i].priceName.toLowerCase() + '</p>' + '</div>'
-            + '<div class="col-md-4"><a id="upgrades-btn-' + i + '" type="button" class="btn btn-primary btn-block" onclick="g.u.buy(' + i + ')">Buy upgrade</a></div>'
-            + '</div>');
+        let main = $(document.createElement("div"));
+        main.attr('id', 'upgrades-row-' + i);
+        main.attr('class', 'row bottom-spacer');
+
+        let infoBox = $('<div class="col-md-8"><p class="no-margin">' + g.u.list[i].name + " : " + g.u.list[i].desc + "<br>Cost " + fix(g.u.list[i].price.amount, 0) + " " + g.u.list[i].price.type.toLowerCase() + '</p>' + '</div>');
+        let buyButton = $('<div class="col-md-4"><a id="upgrades-btn-' + i + '" type="button" class="btn btn-primary btn-block" onclick="g.u.buy(' + i + ')">Buy upgrade</a></div>');
+
+        main.append(infoBox);
+        main.append(buyButton);
+
+        $("#upgrades-panelbody").append(main);
     }
 };
 game.upgrades.hide = function () { // todo
@@ -75,15 +77,15 @@ game.upgrades.checkSave = function () {
 
 g.u.owned = [];
 g.u.list = [
-    new g.u.create("Hydrogen I", "Hydrogen/click x2", 10, 'Hydrogen', 'owned[0]', 'perClick[0]', '*2'),
-    new g.u.create("Hydrogen II", "Hydrogen/click x2", 75, 'Hydrogen', 'owned[0]', 'perClick[0]', '*2'),
-    new g.u.create("Hydrogen III", "Hydrogen/click x1.5", 1000, 'Hydrogen', 'owned[0]', 'perClick[0]', '*1.5'),
+    new g.u.create("Hydrogen I", "Hydrogen/click x2", {amount:10, type:'Hydrogen'}, function(){game.ressources.perClick.Hydrogen.amount *= 2;}),
+    new g.u.create("Hydrogen II", "Hydrogen/click x2", {amount:75, type:'Hydrogen'}, function(){game.ressources.perClick.Hydrogen.amount *= 2;}),
+    new g.u.create("Hydrogen III", "Hydrogen/click x1.5",{amount:1000, type:'Hydrogen'}, function(){game.ressources.perClick.Hydrogen.amount *= 1.5;}),
     
-    new g.u.create("Oxygen I", "Oxygen/click x2", 10, 'Oxygen', 'owned[1]', 'perClick[1]', '*2'),
-    new g.u.create("Oxygen II", "Oxygen/click x2", 75, 'Oxygen', 'owned[1]', 'perClick[1]', '*2'),
-    new g.u.create("Oxygen III", "Oxygen/click x1.5", 1000, 'Oxygen', 'owned[1]', 'perClick[1]', '*1.5'),
+    new g.u.create("Oxygen I", "Oxygen/click x2", {amount:10, type:'Oxygen'}, function(){game.ressources.perClick.Oxygen.amount *= 2;}),
+    new g.u.create("Oxygen II", "Oxygen/click x2", {amount:75, type:'Oxygen'}, function(){game.ressources.perClick.Oxygen.amount *= 2;}),
+    new g.u.create("Oxygen III", "Oxygen/click x1.5", {amount:1000, type:'Oxygen'}, function(){game.ressources.perClick.Oxygen.amount *= 1.5;}),
     
-    new g.u.create("Helium I", "Helium/click x2", 10, 'Helium', 'owned[2]', 'perClick[2]', '*2'),
-    new g.u.create("Helium II", "Helium/click x2", 75, 'Helium', 'owned[2]', 'perClick[2]', '*2'),
-    new g.u.create("Helium III", "Helium/click x1.5", 1000, 'Helium', 'owned[2]', 'perClick[2]', '*1.5')
+    new g.u.create("Helium I", "Helium/click x2", {amount:10, type:'Helium'}, function(){game.ressources.perClick.Helium.amount *= 2;}),
+    new g.u.create("Helium II", "Helium/click x2", {amount:75, type:'Helium'}, function(){game.ressources.perClick.Helium.amount *= 2;}),
+    new g.u.create("Helium III", "Helium/click x1.5", {amount:1000, type:'Helium'}, function(){game.ressources.perClick.Helium.amount *= 1.5;})
 ];
