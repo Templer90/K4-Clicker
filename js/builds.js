584a8b2e04fabb17a18badb56e69b6e388cfb833
ericheinz <eric.Heinz@nfon.com>
2020-02-07 14:47:59 +0100

    slowly removing JQuery

--- js/builds.js
+++ js/builds.js
@@ -17,40 +17,63 @@ game.builds.create = function (name, desc, price, valuePerSec, reward, inflation
 
     this.buyable = (index) => {
         return g.ressources.owned[g.b.list[index].price.type] >= g.b.list[index].buildPrice(index);
+    };
+
+    this.buy = (index) => {
+        let type = this.price.type;
+        g.ressources.owned[type] -= this.buildPrice(index);
     }
 };
 game.builds.init = () => {
+    let panel = document.getElementById('builds-panelbody');
     for (let i = 0; i < g.b.list.length; i++) {
+        let obj = g.b.list[i];
         g.b.owned.push(0);
         g.b.multiplier.push(1);
 
-        let main = $(document.createElement("div"));
-        main.attr('id', 'builds-row-' + i);
-        main.attr('class', 'row bottom-spacer');
+        let main = document.createElement("div");
+        main.setAttribute('id', 'builds-row-' + i);
+        main.setAttribute('class', 'row bottom-spacer');
 
-        let infoBox = $('<div class="col-md-8"><p id="builds-infos-' + i + '" class="no-margin">' + g.b.list[i].name + " : " + g.b.list[i].reward.type + " " + g.b.list[i].price.type.toLowerCase() + "/sec<br>" + g.b.owned[i] + " owned : " + h.buildReward(i) + " " + g.b.list[i].price.type.toLowerCase() + "/sec" + "<br>Cost " + fix(g.b.list[i].buildPrice(i), 0) + " " + g.b.list[i].price.type.toLowerCase() + '</p></div>');
-        let buyButton = $('<div class="col-md-4"><a id="builds-btn-' + i + '" type="button" class="btn btn-primary btn-block" onclick="g.b.buy(' + i + ',\'' + g.b.list[i].name + '\')">Buy build</a></div>');
+        let infoBox = document.createElement("div");
+        infoBox.setAttribute('class', 'col-md-8');
+
+        let paragraph = document.createElement("p");
+        paragraph.id = "builds-infos-" + i;
+        paragraph.setAttribute('class', 'no-margin');
+        paragraph.innerHTML = obj.name + " : " + obj.reward.type + " " + obj.price.type.toLowerCase() + "/sec<br>" + g.b.owned[i] + " owned : " + h.buildReward(i) + " " + obj.price.type.toLowerCase() + "/sec" + "<br>Cost " + fix(obj.buildPrice(i), 0) + " " + obj.price.type.toLowerCase();
+        infoBox.append(paragraph);
+
+        let buyButton = document.createElement("div");
+        buyButton.setAttribute('class', 'col-md-4');
+        let buyLink = document.createElement("a");
+        buyLink.id = 'builds-btn-' + obj.name;
+        buyLink.setAttribute('class', 'btn btn-primary btn-block');
+        buyLink.setAttribute('type', 'button');
+        buyLink.onclick = () => {
+            g.b.buy(i, obj.name);
+        };
+        buyLink.innerHTML = 'Buy build';
+        buyButton.append(buyLink);
 
         main.append(infoBox);
         main.append(buyButton);
 
-        $("#builds-panelbody").append(main);
+        panel.append(main);
     }
 };
 game.builds.buy = (index, name) => {
-    let price = g.b.list[index].price.amount;
-    let type = g.b.list[index].price.type;
-
     if (g.b.list[index].buyable(index)) {
-        g.ressources.owned[type] -= price;
+        g.b.list[index].buy(index);
         g.b.owned[index]++;
         g.b.update();
     }
 };
 game.builds.earn = (times) => {
+    let delta = times / g.options.fps;
     for (let i = 0; i < g.b.list.length; i++) {
         if (g.b.owned[i] > 0) {
-            g.b.list[i].reward(g.b.list[i].valuePerSec * g.b.owned[i] * g.b.multiplier[i], times / g.options.fps);
+            g.b.list[i].reward(g.b.list[i].valuePerSec * g.b.owned[i] * g.b.multiplier[i], delta);
         }
     }
 };
@@ -64,11 +87,10 @@ game.builds.checkSave = () => {
 game.builds.update = () => {
     for (let i = 0; i < g.b.list.length; i++) {
         let obj = g.b.list[i];
-        let string = obj.name + " : " + fix(obj.valuePerSec, 2) + " " + obj.price.type.toLowerCase() + "/sec<br>" +
-            fix(g.b.owned[i], 0) + " owned : " + fix(obj.valuePerSec * g.b.owned[i] * g.b.multiplier[i], 2) + " " + obj.price.type.toLowerCase() + "/sec" + "<br>" +
-            "Cost " + fix(obj.buildPrice(i), 0) + " " + obj.price.type.toLowerCase();
-
-        $("#builds-infos-" + i).html(string);
+        let line1 = obj.name + " : " + fix(obj.valuePerSec, 2) + " " + obj.price.type.toLowerCase() + "/sec";
+        let line2 = fix(g.b.owned[i], 0) + " owned : " + fix(obj.valuePerSec * g.b.owned[i] * g.b.multiplier[i], 2) + " " + obj.price.type.toLowerCase() + "/sec";
+        let line3 = "Cost " + fix(obj.buildPrice(i), 0) + " " + obj.price.type.toLowerCase();
+        document.getElementById("builds-infos-" + i).innerHTML = line1 + "<br>" + line2 + "<br>" + line3 + "<br>";
     }
 };
 
--- js/upgrades.js
+++ js/upgrades.js
@@ -1,73 +1,104 @@
 g.upgrades = g.u = {};
-g.u.owned = [];
+g.u.owned = {};
 
-game.upgrades.create = function (name, desc, price, buyFunction) {
+game.upgrades.create = function (name, desc, price, boughtFunction, dependsOn = undefined) {
     this.name = name;
     this.desc = desc;
     this.price = price;
-    this.func = buyFunction;
+    this.boughtFunction = boughtFunction;
+    this.depends = () => {
+        return true;
+    };
 
-    this.buyable = (index) => {
-        return g.ressources.owned[this.price.type] >= this.price.amount && g.u.owned[index] === false;
+    if (dependsOn !== undefined) {
+        this.depends = dependsOn;
     }
 };
 
-game.upgrades.buy = (i) => {
-    let obj = g.u.list[i];
+game.upgrades.buyable = (name) => {
+    let obj = g.u.list[name];
+    return g.ressources.owned[obj.price.type] >= obj.price.amount && g.u.owned[name] === false;
+};
 
-    if (obj.buyable(i)) {
+game.upgrades.buy = (name) => {
+    let obj = g.u.list[name];
+
+    if (game.upgrades.buyable(name) && obj.depends()) {
         g.ressources.owned[obj.price.type] -= obj.price.amount;
-        obj.func();
-        g.u.owned[i] = true;
+        obj.boughtFunction();
+        g.u.owned[name] = true;
         g.buttons();
 
-        let upgradeBTN = $("#upgrades-btn-" + i);
-        upgradeBTN.attr('onclick', '');
-        upgradeBTN.removeClass('btn-primary');
-        upgradeBTN.addClass('btn-success');
-        upgradeBTN.html('Owned');
+        let upgradeBTN = document.getElementById("upgrades-btn-" + name);
+        upgradeBTN.setAttribute('onclick', '');
+        upgradeBTN.classList.replace('btn-primary', 'btn-success');
+        upgradeBTN.innerHTML = 'Owned';
         g.u.hide();
     }
 };
 
 game.upgrades.init = () => {
+    let panel = document.getElementById('upgrades-panelbody');
     for (let i = 0; i < g.u.list.length; i++) {
-        g.u.owned.push(false);
-        let main = $(document.createElement("div"));
-        main.attr('id', 'upgrades-row-' + i);
-        main.attr('class', 'row bottom-spacer');
+        let obj = g.u.list[i];
+        g.u.list[obj.name] = obj;
+        g.u.owned[obj.name] = false;
 
-        let infoBox = $('<div class="col-md-8"><p class="no-margin">' + g.u.list[i].name + " : " + g.u.list[i].desc + "<br>Cost " + fix(g.u.list[i].price.amount, 0) + " " + g.u.list[i].price.type.toLowerCase() + '</p>' + '</div>');
-        let buyButton = $('<div class="col-md-4"><a id="upgrades-btn-' + i + '" type="button" class="btn btn-primary btn-block" onclick="g.u.buy(' + i + ')">Buy upgrade</a></div>');
+        let main = document.createElement("div");
+        main.setAttribute('id', 'upgrades-row-' + i);
+        main.setAttribute('class', 'row bottom-spacer');
+
+        let infoBox = document.createElement("div");
+        infoBox.setAttribute('class', 'col-md-8');
+
+        let paragraph = document.createElement("p");
+        paragraph.setAttribute('class', 'no-margin');
+        paragraph.innerHTML = obj.name + " : " + obj.desc + "<br>Cost " + fix(obj.price.amount, 0) + " " + obj.price.type.toLowerCase();
+        infoBox.append(paragraph);
+
+        let buyButton = document.createElement("div");
+        buyButton.setAttribute('class', 'col-md-4');
+        let buyLink = document.createElement("a");
+        buyLink.id = 'upgrades-btn-' + obj.name;
+        buyLink.setAttribute('class', 'btn btn-primary btn-block');
+        buyLink.setAttribute('type', 'button');
+        buyLink.onclick = () => {
+            g.u.buy(obj.name);
+        };
+        buyLink.innerHTML = 'Buy upgrade';
+
+        buyButton.append(buyLink);
 
         main.append(infoBox);
         main.append(buyButton);
 
-        $("#upgrades-panelbody").append(main);
+        panel.append(main);
     }
 };
 game.upgrades.hide = () => { // todo
     let a = document.getElementById('upgrades-checkbox');
     if (a.checked === true) {
         for (let i = 0; i < g.u.list.length; i++) {
-            if (g.u.owned[i] === true)
-                $("#upgrades-row-" + i).css("display", 'block');
+            let obj = g.u.list[i];
+            if (g.u.owned[obj.name] === true)
+                document.getElementById("upgrades-row-" + i).style.display = 'block';
         }
     } else {
         for (let i = 0; i < g.u.list.length; i++) {
-            if (g.u.owned[i] === true)
-                $("#upgrades-row-" + i).css("display", 'none');
+            let obj = g.u.list[i];
+            if (g.u.owned[obj.name] === true)
+                document.getElementById("upgrades-row-" + i).style.display = 'none';
         }
     }
 };
 game.upgrades.check = () => {
     for (let i = 0; i < g.u.list.length; i++) {
-        if (g.u.owned[i] === true) {
-            let upgradeBTN = $("#upgrades-btn-" + i);
-            upgradeBTN.attr('onclick', '');
-            upgradeBTN.removeClass('btn-primary');
-            upgradeBTN.addClass('btn-success');
-            upgradeBTN.html('Owned');
+        let obj = g.u.list[i];
+        if (g.u.owned[obj.name] === true) {
+            let upgradeBTN = document.getElementById("upgrades-btn-" + i);
+            upgradeBTN.setAttribute('onclick', '');
+            upgradeBTN.classList.replace('btn-primary', 'btn-success');
+            upgradeBTN.innerHTML = 'Owned';
         }
     }
 };
@@ -79,7 +110,6 @@ game.upgrades.checkSave = () => {
     }
 };
 
-g.u.owned = [];
 g.u.list = [
     new g.u.create("Hydrogen I", "Hydrogen/click x2", {amount: 10, type: 'Hydrogen'}, () => {
         game.ressources.perClick.Hydrogen.amount *= 2;
@@ -89,6 +119,15 @@ g.u.list = [
     }),
     new g.u.create("Hydrogen III", "Hydrogen/click x1.5", {amount: 1000, type: 'Hydrogen'}, () => {
         game.ressources.perClick.Hydrogen.amount *= 1.5;
+    }),
+
+    new g.u.create("Hydrogen Manufacture", "Auto Hydrogen=clicks", {amount: 2000, type: 'Hydrogen'}, () => {
+        for (let i = 0; i < g.b.multiplier.length; i++) {
+            game.builds.multiplier[i] *= game.ressources.perClick.Hydrogen.amount;
+            game.builds.update();
+        }
+    }, () => {
+        return g.u.owned["Hydrogen III"];
     }),
 
     new g.u.create("Oxygen I", "Oxygen/click x2", {amount: 10, type: 'Oxygen'}, () => {
