fddab47cc9ee4b6bdaebe3f84302c8d761d7f683
eric <ericTempler@googlemail.com>
2020-03-16 21:01:44 +0100

    Migrated to Linux

--- data/achievements.js
+++ data/achievements.js
@@ -1,5 +1,8 @@
 g.achievements.list = [
-    new Achievement('Test 1', 'test', () => game.resources.total.Hydrogen >= 1000, () => {
+    new Achievement('First Steps', 'test', () => game.resources.total.Hydrogen >= 1000, () => {
         console.log("Yay 1000 Hydrogen");
+    }),
+    new Achievement('Avogadro', `More than ${elements.avogadro} Atoms`, () => game.resources.total.Hydrogen >= elements.avogadro, () => {
+        console.log("avogadro");
     }),
 ];
