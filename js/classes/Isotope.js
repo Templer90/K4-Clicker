b238dff0a85559d1a36407b50acda6be4641d275
Eric Templer <ericTempler@googlemail.com>
2020-04-10 22:54:08 +0200

    Isotope Fusion is working

--- js/classes/Isotope.js
+++ js/classes/Isotope.js
@@ -1,7 +1,7 @@
 class Isotope {
-    constructor(protons, neutrons= 0, energy= 0) {
+    constructor(protons, neutrons = 0, energy = 0) {
         if (protons === 0 && neutrons === 0) {
-            //Energy? Radiation?
+            //Energy? Radiation? debug?
         }
 
         this.protons = protons;
@@ -13,17 +13,19 @@ class Isotope {
 
         if (protons === 1) {
             let element;
-            if (neutrons === 1) {
+            if (neutrons === 0) {
+                element = elements.find("Hydrogen");
+            } else if (neutrons === 1) {
                 element = elements.find("Deuterium");
             } else if (neutrons === 2) {
                 element = elements.find("Tritium");
             } else {
-                element = elements.find("Hydrogen");
+                return;
             }
             this.name = element.name;
             this.symbol = element.symbol;
             this.atomicMass = element["atomic_mass"];
-            this.massNumber = element["#m"];
+            this.massNumber = element.isotopes[0]["#m"];
         } else if (protons === 0 && neutrons === 1) {
             let element = elements.find("Neutron");
 
@@ -37,7 +39,7 @@ class Isotope {
             });
 
             if (element === undefined) {
-                console.error('could NOT find Element', protons, neutrons);
+                console.log('could NOT find Element', protons, neutrons);
                 return;
             }
 
@@ -50,7 +52,7 @@ class Isotope {
                     return is["#m"] === (neutrons + protons);
                 });
                 if (iso === undefined) {
-                    console.error('could NOT find Isotope', protons, neutrons);
+                    console.log('could NOT find Isotope', protons, neutrons);
                     return;
                 }
 
@@ -58,18 +60,58 @@ class Isotope {
                 this.massNumber = iso["#m"];
                 this.neutrons = neutrons;
             }
-            
+
             this.name = element.name;
             this.symbol = element.symbol;
         }
     }
 
-    massDifference() {
-        return (this.atomicMass - (this.protons * 1.007276466583 + this.neutrons * 1.00866491595)) * elements.meVPerU;
+    massDifferenceEnergy() {
+        return this.massDeficit() * elements.meVPerU;
     }
 
+    massDeficit() {
+        return (this.protons * 1.007276466583 + this.neutrons * 1.00866491595) - this.atomicMass;
+    }
+
+    nuclearBindingEnergy() {
+        return this.massDifferenceEnergy();
+    }
     
-    static fuseIsotopes(isotope_a, isotope_b, energy_a, energy_b) {
+    static Hydrogen() {
+        return new Isotope(1, 0);
+    }
+
+    static Deuterium() {
+        return new Isotope(1, 1);
+    }
+
+    static Tritium() {
+        return new Isotope(1, 2);
+    }
+    
+    static BetaRadiation() {
+        return new Isotope(2, 2);
+    }
+
+    static Helium() {
+        return new Isotope(2, 2);
+    }
+
+    static AlphaRadiation() {
+        return new Isotope(2, 2);
+    }
+
+    static isBalanced(input, output) {
+        const inputP = input.reduce((acc, cur) => acc + cur.protons, 0);
+        const inputMass = input.reduce((acc, cur) => acc + cur.massNumber, 0);
+        const outputP = output.reduce((acc, cur) => acc + cur.protons, 0);
+        const outMass = output.reduce((acc, cur) => acc + cur.massNumber, 0);
+
+        return (inputP === outputP) && ((inputMass === outMass));
+    }
+
+    static fuseIsotopes(isotope_a, isotope_b, energy_a = 0, energy_b = 0) {
         // http://hyperphysics.phy-astr.gsu.edu/hbase/NucEne/coubar.html
         let coulomb_barrier = (iso1, iso2 = undefined) => {
             const mul = 1.43997218e-15; //1.43997218e-9;  e²/(4*pi*permeability of vacuum); because I want MeV i divide by a million 
@@ -96,27 +138,91 @@ class Isotope {
         let initialEnergy = energy_a + energy_b;
         let initialProtons = isotope_a.protons + isotope_b.protons;
         let initialNeutrons = isotope_a.neutrons + isotope_b.neutrons;
-        
+
+        let decayProducts = [];
+
+        // beta-plus-decay
         const transmutationEnergy = 1.29;
         while (initialProtons > 1 && initialEnergy >= transmutationEnergy) {
             initialProtons -= 1;
             initialNeutrons += 1;
             initialEnergy -= transmutationEnergy;
+            decayProducts.push(new Positron());
         }
 
-
-
-        const theorecticalFusion = new Isotope(initialProtons, initialNeutrons);
-        
         //fusion
+        let intermediateFusionProduct = new Isotope(initialProtons, initialNeutrons);
+        let fusionEnergy = intermediateFusionProduct.massDifferenceEnergy() + initialEnergy;
+
+        //Decay
+        let chosenDecay;
+
+        for (let i = 0; (i < 10) && Isotope.isBalanced([isotope_a, isotope_b], decayProducts) === false; i++) {
+            const potentialDecays = [new Neutron(), Isotope.Hydrogen(), Isotope.AlphaRadiation()];
+            let decays = potentialDecays.map((eject) => {
+                const protonDiff = intermediateFusionProduct.protons - eject.protons;
+                const neutronDiff = intermediateFusionProduct.neutrons - eject.neutrons;
+
+                if (protonDiff === 0 && neutronDiff !== 1) {
+                    return undefined;
+                }
+                if (protonDiff < -1 || neutronDiff < 0) {
+                    return undefined;
+                }
+                if (protonDiff > 1 && neutronDiff <= 0) {
+                    return undefined;
+                }
+
+
+                let product = new Isotope(protonDiff, neutronDiff);
+                let E = (intermediateFusionProduct.atomicMass - product.atomicMass - eject.atomicMass) * elements.meVPerU;
+
+                return {
+                    energy: E,
+                    eject: eject,
+                    product: product
+                };
+            });
+
+            //remove invalid entries
+            decays = decays.filter((a) => {
+                return a !== undefined
+            }).filter((a) => {
+                return a.eject.name !== 'Unknown'
+            }).filter((a) => {
+                return a.product.name !== 'Unknown'
+            });
+
+            //add the "no Decay" aka the isotope stays as is
+            decays.push({
+                energy: 0,
+                eject: undefined,
+                product: intermediateFusionProduct
+            });
+
+            //remove invalid entries
+            decays = decays.sort((a, b) => {
+                return (a.energy - b.energy) - fusionEnergy;
+            });
+
+            chosenDecay = decays[0];
+
+            if (chosenDecay.eject === undefined) {
+                decayProducts.push(chosenDecay.product);
+                break;
+            } else {
+                decayProducts.push(chosenDecay.eject);
+            }
+        }
 
         return {
             type: 'debug',
-            resultIsotopes: [theorecticalFusion],
-            energy: 0
-        }
+            resultIsotopes: decayProducts,
+            energy: fusionEnergy + chosenDecay.energy,
+            deltaEnergy: (fusionEnergy + chosenDecay.energy) - (energy_a + energy_b),
+            test: Isotope.isBalanced([isotope_a, isotope_b], decayProducts)
+        };
     }
-
 
     static massDeficit(isotope) {
         const num = elem['#m'];
@@ -132,5 +238,37 @@ class Isotope {
             combined: num + neu,
             diff: (theoEn / realEn) * 100
         }
+    }
+}
+
+class Neutron extends Isotope {
+    constructor() {
+        super(0, 1);
+    }
+}
+
+class Electron extends Isotope {
+    constructor() {
+        super(0, 0, 0);
+
+        this.name = 'Electron';
+        this.symbol = 'e';
+        this.protons = -1;
+        this.neutrons = 0;
+        this.atomicMass = 0;
+        this.massNumber = 0;
+    }
+}
+
+class Positron extends Isotope {
+    constructor() {
+        super(0, 0, 0);
+
+        this.name = 'Positron';
+        this.symbol = 'e+';
+        this.protons = 1;
+        this.neutrons = 0;
+        this.atomicMass = 0;
+        this.massNumber = 0;
     }
 }
--- js/debug.js
+++ js/debug.js
@@ -1,5 +1,18 @@
 const debug = {};
 
+debug.protonChain = (a, b) => {
+    console.log("Proton + Proton => Deuterium 1,442 MeV");
+    const deuterium = Isotope.fuseIsotopes(Isotope.Hydrogen(), Isotope.Hydrogen(), 1, 1);
+    console.log(deuterium, deuterium.resultIsotopes.map((a) => a.name));
+    console.log("Deuterium + Proton => Helium-3 5,49 MeV");
+    const helium3 = Isotope.fuseIsotopes(Isotope.Hydrogen(), Isotope.Deuterium(), a, b);
+    console.log(helium3, helium3.resultIsotopes.map((a) => a.name));
+
+    console.log("Helium-3 + Helium-3 => Helium + 2Protons");
+    const helium = Isotope.fuseIsotopes(new Isotope(2, 1), new Isotope(1, 0), 1,1);
+    console.log(helium, helium.resultIsotopes.map((a) => a.name))
+};
+
 debug.builds = () => {
     game.builds.list.forEach((b) => {
         b.visible = true;
@@ -17,7 +30,6 @@ debug.dev = (value = undefined) => {
     }
     localStorage.setItem(save.devKey, value);
 };
-
 debug.noDev = () => {
     localStorage.removeItem(save.devKey);
 };
